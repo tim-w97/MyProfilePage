b861e16c2e6ec3e1b8204852e5989d9843017378
Tim <timwagner997@gmail.com>
2022-12-19 01:55:19 +0100

    added a shadow to the menu

--- src/widgets/Menu.js
+++ src/widgets/Menu.js
@@ -1,7 +1,7 @@
 function Menu() {
     // TODO: Show my full name here.
     return (
-        <div className="bg-tim-black text-tim-green p-3 font-mono">
+        <div className="bg-tim-black text-tim-green p-3 font-mono drop-shadow-md">
             <ul>
                 <li className="text-2xl text-right m-5">Über mich</li>
                 <li className="text-2xl text-right m-5">Fähigkeiten</li>
