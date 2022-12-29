2192941d51eea6690a6913810468bf0ed2593559
Tim <timwagner997@gmail.com>
2022-12-29 22:52:55 +0100

    changed the icons of the top bar

--- src/widgets/icons/XMark.js
+++ src/widgets/icons/XMark.js
@@ -1,6 +1,6 @@
 function XMark() {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-16 stroke-tim-green m-3">
+        <svg className="h-full p-1 stroke-tim-green" viewBox="0 0 24 24" strokeWidth={3}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
         </svg>
     );
