fec5c1569c647e0ab58dea21d9e87f2c35131d8b
Tim <timwagner997@gmail.com>
2023-01-02 00:28:25 +0100

    added back button to the cv

--- src/pages/CVPage.js
+++ src/pages/CVPage.js
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom";
+import Back from "../widgets/icons/Back";
+
 function CVPage() {
   return (
     <div className="font-mono md:mx-20">
@@ -10,8 +13,14 @@ function CVPage() {
       </div>
       <div className="fixed top-0 left-0 right-0">
         <div className="md:mx-20">
-          <div className="flex justify-center items-center h-20 max-w-screen-md m-auto bg-tim-green drop-shadow-md">
-            <p className="text-2xl font-bold text-tim-gray">Lebenslauf</p>
+          <div className="flex items-center h-20 max-w-screen-md m-auto bg-tim-green drop-shadow-md">
+            <Link to="/">
+              <Back className="fill-tim-gray w-20 aspect-square p-2" />
+            </Link>
+            <p className="grow text-2xl font-bold text-tim-gray text-center">
+              Lebenslauf
+            </p>
+            <div className="w-20 aspect-square"></div>
           </div>
         </div>
       </div>
