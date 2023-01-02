f2cba85c171f35ee1ed7aaf5c9591fb5a803d126
Tim <timwagner997@gmail.com>
2023-01-02 22:14:25 +0100

    adjsuted cv

--- src/pages/CVPage.js
+++ src/pages/CVPage.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import Divider from "../widgets/Divider";
 import Back from "../widgets/icons/Back";
 
 function CVPage() {
@@ -18,9 +19,11 @@ function CVPage() {
             <span className="font-bold">Mobile Computing</span> im Bachelor und
             spezialisiere mich auf die Entwicklung von Apps für Android und iOS.
           </p>
+        </div>
 
-          <div className="bg-tim-green h-0.5 my-8"></div>
+        <Divider />
 
+        <div className="mx-5 md:mx-0">
           <p className="text-tim-gray">Seit November 2022</p>
           <p className="text-xl my-3">
             Hochschule für Angewandte Wissenschaften Hof
@@ -30,9 +33,11 @@ function CVPage() {
             <span className="font-bold">Tutor</span> und bringe unseren
             Informatik-Erstsemestern das Programmieren in Java bei.
           </p>
+        </div>
 
-          <div className="bg-tim-green h-0.5 my-8"></div>
+        <Divider />
 
+        <div className="mx-5 md:mx-0">
           <p className="text-tim-gray">September 2021 bis Juni 2022</p>
           <p className="text-xl my-3">dot9 GmbH</p>
           <p>
@@ -40,9 +45,11 @@ function CVPage() {
             <span className="font-bold">Werkstudent</span> tätig und habe
             während dieser Zeit größenteils Webseiten mit React entwickelt.
           </p>
+        </div>
 
-          <div className="bg-tim-green h-0.5 my-8"></div>
+        <Divider />
 
+        <div className="mx-5 md:mx-0">
           <p className="text-tim-gray">Januar 2021 bis Juli 2021</p>
           <p className="text-xl my-3">IBYKUS AG</p>
           <p>
@@ -50,12 +57,14 @@ function CVPage() {
             <span className="font-bold">Softwareentwickler</span> gearbeitet und
             Webseiten mit dem JavaServer Faces-Framework entwickelt.
           </p>
+        </div>
 
-          <div className="bg-tim-green h-0.5 my-8"></div>
+        <Divider />
 
+        <div className="mx-5 md:mx-0">
           <p className="text-tim-gray">August 2018 bis Januar 2021</p>
           <p className="text-xl my-3">IBYKUS AG</p>
-          <p className="mb-6">
+          <p className="mb-8">
             Bei der IBYKUS AG habe ich meine Ausbildung zum{" "}
             <span className="font-bold">
               Fachinformatiker für Anwendungsentwicklung
---
+++ src/widgets/Divider.js
@@ -0,0 +1,5 @@
+function Divider() {
+  return <div className="bg-tim-green h-0.5 my-8"></div>;
+}
+
+export default Divider;
