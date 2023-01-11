391f21c1c10501e5412fda9eb0f05560678af43e
Tim <timwagner997@gmail.com>
2023-01-11 13:34:38 +0100

    added shrink and grow properties to Location icon to fix its size

--- src/widgets/CVEntry.js
+++ src/widgets/CVEntry.js
@@ -6,7 +6,7 @@ function CVEntry(props) {
       <p className="text-tim-gray">{props.time}</p>
       <p className="text-xl mt-3">{props.position}</p>
       <div className="flex items-center my-3">
-        <Location className="w-8 mr-1" />
+        <Location className="w-8 mr-1 shrink-0 grow-0" />
         <p className="text-tim-gray">{props.location}</p>
       </div>
       {props.description}
