bee52991638ad21f1fc979ea2b9d106d5f06d39c
Tim <timwagner997@gmail.com>
2023-01-11 13:45:00 +0100

    prevented breaking of my Name on the welcome page / formatted document

--- src/widgets/Welcome.js
+++ src/widgets/Welcome.js
@@ -1,22 +1,38 @@
-import timAtWaterStation from '../assets/images/tim-at-water-station.png';
+import timAtWaterStation from "../assets/images/tim-at-water-station.png";
 
 function Welcome(props) {
-    return (
-        <div className={props.className}>
-            <div id='anchor-aboutme' className="absolute -top-48"></div>
-            <p className="text-2xl font-bold">Hey, mein Name ist Tim. 👨🏻‍💻</p>
+  return (
+    <div className={props.className}>
+      <div id="anchor-aboutme" className="absolute -top-48"></div>
+      <p className="text-2xl font-bold">
+        Hi, mein Name ist{" "}
+        <span className="font-bold whitespace-nowrap">Tim. 🙋🏻‍♂️</span>
+      </p>
 
-            <p className="text-xl mt-6">Ich studiere <span className='font-bold whitespace-nowrap'>Mobile Computing</span> an der Hochschule für Angewandte Wissenschaften in Hof.</p>
+      <p className="text-xl mt-6">
+        Ich studiere{" "}
+        <span className="font-bold whitespace-nowrap">Mobile Computing</span> an
+        der Hochschule für Angewandte Wissenschaften in Hof.
+      </p>
 
-            <p className='mt-3'>In meinem Studium geht es neben der allgemeinen Informatik vor allem um eins - um die Programmierung von Apps.</p>
+      <p className="mt-3">
+        In meinem Studium geht es neben der allgemeinen Informatik vor allem um
+        eins - um die Programmierung von Apps.
+      </p>
 
-            <img src={timAtWaterStation} alt="Tim at a water station"></img>
+      <img src={timAtWaterStation} alt="Tim at a water station"></img>
 
-            <p className='mt-6'>Denn App-Programmierung ist genau das, was ich nach meinem Studium hauptberuflich machen will.</p>
+      <p className="mt-6">
+        Warum ich genau diesen Studiengang gewählt habe? Weil ich nach meinem
+        Studium als App-Entwickler arbeiten will. 👨🏻‍💻📱
+      </p>
 
-            <p className='mt-3'>Das Flutter-Framework interessiert mich am meisten, doch ich habe auch mit anderen Tools bereits Erfahrungen gesammelt.</p>
-        </div>
-    );
+      <p className="mt-3">
+        Das Flutter-Framework interessiert mich am meisten, doch ich habe auch
+        mit anderen Tools bereits Erfahrungen gesammelt:
+      </p>
+    </div>
+  );
 }
 
 export default Welcome;
