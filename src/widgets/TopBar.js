0266426c8d823658d2d52d08db399a92c77a75b6
Tim <timwagner997@gmail.com>
2023-01-12 17:02:02 +0100

    burger icon didn't appear on some browsers - fixed this issue :)

--- src/widgets/TopBar.js
+++ src/widgets/TopBar.js
@@ -4,31 +4,38 @@ import XMark from "./icons/XMark";
 import Menu from "./Menu";
 
 function TopBar() {
-    const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    function toggleMenu() {
-        setMenuOpen(!menuOpen);
-    }
+  function toggleMenu() {
+    setMenuOpen(!menuOpen);
+  }
 
-    function closeMenu() {
-        setMenuOpen(false);
-    }
+  function closeMenu() {
+    setMenuOpen(false);
+  }
 
-    return (
-        <div className="fixed top-0 left-0 right-0">
-            <div className="md:mx-20">
-                <div className="flex justify-end h-20 max-w-screen-md m-auto bg-tim-gray drop-shadow-md">
-                    <button onClick={toggleMenu} className={`transition-transform duration-300 ${menuOpen ? '-rotate-90' : null}`}>
-                        {menuOpen ?
-                            <XMark /> :
-                            <Burger />
-                        }
-                    </button>
-                </div>
-                <Menu className={`transition-transform duration-300 ${menuOpen ? 'scale-100' : 'scale-0 h-0'}`} onMenuClose={closeMenu} />
-            </div >
+  return (
+    <div className="fixed top-0 left-0 right-0">
+      <div className="md:mx-20">
+        <div className="flex justify-end h-20 max-w-screen-md m-auto bg-tim-gray drop-shadow-md">
+          <button
+            onClick={toggleMenu}
+            className={`h-full aspect-square transition-transform duration-300 ${
+              menuOpen ? "-rotate-90" : null
+            }`}
+          >
+            {menuOpen ? <XMark /> : <Burger />}
+          </button>
         </div>
-    );
+        <Menu
+          className={`transition-transform duration-300 ${
+            menuOpen ? "scale-100" : "scale-0 h-0"
+          }`}
+          onMenuClose={closeMenu}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default TopBar;
--- src/widgets/icons/XMark.js
+++ src/widgets/icons/XMark.js
@@ -1,9 +1,17 @@
-function XMark() {
-    return (
-        <svg className="h-full p-1 stroke-tim-green" viewBox="0 0 24 24" strokeWidth={3}>
-            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-        </svg>
-    );
+function XMark(props) {
+  return (
+    <svg
+      className={props.className + " stroke-tim-green"}
+      viewBox="0 0 24 24"
+      strokeWidth={3}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 18L18 6M6 6l12 12"
+      />
+    </svg>
+  );
 }
 
 export default XMark;
