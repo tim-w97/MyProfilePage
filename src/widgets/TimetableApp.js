046bb018dc79d4db8d5677fff5d8403ae48ea558
Tim <timwagner997@gmail.com>
2024-01-24 23:57:29 +0100

    added custom css for navigation buttons

--- src/widgets/TimetableApp.js
+++ src/widgets/TimetableApp.js
@@ -3,9 +3,41 @@ import timetableAppLectures from "../assets/images/apps/timetable-app/my lecture
 import timetableAppSettings from "../assets/images/apps/timetable-app/settings screen.png";
 import timetableAppSelectCourse from "../assets/images/apps/timetable-app/select course.png";
 import timetableAppSelectLectures from "../assets/images/apps/timetable-app/select lectures.png";
+import {useEffect, useRef} from "react";
 
 function TimetableApp(props) {
     const imageStyle = 'w-80 m-auto p-3'
+
+    const swiperRef = useRef(null);
+
+    useEffect(() => {
+        const swiperContainer = swiperRef.current;
+
+        const params = {
+            navigation: true,
+            injectStyles: [`              
+              .swiper-button-next,
+              .swiper-button-prev {
+                background-color: #7BC7F3;
+                border-radius: 100%;
+                color: #181A1C;
+                transform: scale(0.8);
+              }
+              
+              .swiper-button-next {
+                padding: 10px 15px 10px 21px;
+              }
+              
+              .swiper-button-prev {
+                padding: 10px 21px 10px 15px;
+              }
+              `,
+            ],
+        };
+
+        Object.assign(swiperContainer, params);
+        swiperContainer.initialize();
+    }, []);
     
     return(
         <div className={props.className}>
@@ -14,7 +46,7 @@ function TimetableApp(props) {
                 Hochschule erstellt.
             </p>
 
-            <swiper-container navigation="true">
+            <swiper-container ref={swiperRef} init="false">
                 <swiper-slide>
                     <img className={imageStyle} src={timetableAppOnboarding} alt='Stundenplan-App Onboarding'/>
                 </swiper-slide>
