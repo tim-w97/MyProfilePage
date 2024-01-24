cb7518895dee372fd3cce6fde8d010625e7f0dee
Tim <timwagner997@gmail.com>
2024-01-25 00:16:38 +0100

    used new Component

--- src/widgets/Eisenpower.js
+++ src/widgets/Eisenpower.js
@@ -3,33 +3,22 @@ import eisenpowerInput from "../assets/images/apps/eisenpower/input.png";
 import eisenpowerImportance from "../assets/images/apps/eisenpower/importance.png";
 import eisenpowerHelpScreen from "../assets/images/apps/eisenpower/help screen.png";
 import eisenpowerSnackbar from "../assets/images/apps/eisenpower/snack bar.png";
+import AppGallery from "./AppGallery";
 
 function Eisenpower(props) {
-    const imageStyle = 'w-80 m-auto p-3'
-    
     return(
         <div className={props.className}>
             <p className='mb-3'>
                 Eisenpower ist eine App, die Todos mithilfe der Eisenhower priorisieren kann.
             </p>
 
-            <swiper-container navigation="true">
-                <swiper-slide>
-                    <img className={imageStyle} src={eisenpowerTodos} alt='Eisenhower Todos'/>
-                </swiper-slide>
-                <swiper-slide>
-                    <img className={imageStyle} src={eisenpowerInput} alt='Eisenhower Add Todo'/>
-                </swiper-slide>
-                <swiper-slide>
-                    <img className={imageStyle} src={eisenpowerImportance} alt='Eisenhower Importance'/>
-                </swiper-slide>
-                <swiper-slide>
-                    <img className={imageStyle} src={eisenpowerHelpScreen} alt='Eisenhower Help Screen'/>
-                </swiper-slide>
-                <swiper-slide>
-                    <img className={imageStyle} src={eisenpowerSnackbar} alt='Eisenhower Snackbar'/>
-                </swiper-slide>
-            </swiper-container>
+            <AppGallery imagePaths={[
+                eisenpowerTodos,
+                eisenpowerInput,
+                eisenpowerImportance,
+                eisenpowerHelpScreen,
+                eisenpowerSnackbar
+            ]}/>
         </div>
     )
 }
--- src/widgets/PacmansBox.js
+++ src/widgets/PacmansBox.js
@@ -3,32 +3,22 @@ import pacmansBoxHold from "../assets/images/apps/pacmans-box/hold screen.png";
 import pacmansBoxRelease from "../assets/images/apps/pacmans-box/release to shoot.png";
 import pacmansBoxFlying from "../assets/images/apps/pacmans-box/flying.png";
 import pacmansBoxGameOver from "../assets/images/apps/pacmans-box/game over.png";
+import AppGallery from "./AppGallery";
 
 function PacmansBox(props) {
-    const imageStyle = 'w-80 m-auto p-3'
-
     return(
         <div className={props.className}>
             <p className='mb-3'>
                 Pacman's Box ist ein Spiel, das ich mit Godot entwickelt habe.
             </p>
-            <swiper-container navigation="true">
-                <swiper-slide>
-                    <img className={imageStyle} src={pacmansBoxMenu} alt='Pacmans Box Menu'/>
-                </swiper-slide>
-                <swiper-slide>
-                    <img className={imageStyle} src={pacmansBoxHold} alt='Pacmans Box Hold screen'/>
-                </swiper-slide>
-                <swiper-slide>
-                    <img className={imageStyle} src={pacmansBoxRelease} alt='Pacmans Box Release screen'/>
-                </swiper-slide>
-                <swiper-slide>
-                    <img className={imageStyle} src={pacmansBoxFlying} alt='Pacmans Box Pacman flying'/>
-                </swiper-slide>
-                <swiper-slide>
-                    <img className={imageStyle} src={pacmansBoxGameOver} alt='Pacmans Box Game over'/>
-                </swiper-slide>
-            </swiper-container>
+
+            <AppGallery imagePaths={[
+                pacmansBoxMenu,
+                pacmansBoxHold,
+                pacmansBoxRelease,
+                pacmansBoxFlying,
+                pacmansBoxGameOver
+            ]}/>
         </div>
     )
 }
