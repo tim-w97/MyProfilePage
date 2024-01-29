1daf697a4cc67a30172d4df1b2229da78df96203
Tim <timwagner997@gmail.com>
2024-01-29 23:30:26 +0100

    changed design of Divider

--- src/widgets/Divider.js
+++ src/widgets/Divider.js
@@ -1,5 +1,7 @@
 function Divider() {
-  return <div className="bg-tim-light-blue h-[1px] my-8"></div>;
+  return (
+      <div className="bg-tim-light-blue h-[1px] my-8 mx-0 md:mx-5 opacity-50"></div>
+  )
 }
 
 export default Divider;
