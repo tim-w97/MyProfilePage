6dfc7dfcea295f2ff864824c6555173ad98a71d7
Tim <timwagner997@gmail.com>
2024-02-01 19:55:34 +0100

    changed swiper variant

--- src/widgets/AppGallery.js
+++ src/widgets/AppGallery.js
@@ -1,41 +1,9 @@
-import {useEffect, useRef} from "react";
-
 function AppGallery(props) {
     const imageStyle = 'w-80 m-auto p-3'
-    const swiperRef = useRef(null);
-
-    useEffect(() => {
-        const swiperContainer = swiperRef.current;
-
-        const params = {
-            navigation: true,
-            injectStyles: [`              
-              .swiper-button-next,
-              .swiper-button-prev {
-                background-color: #7BC7F3;
-                border-radius: 100%;
-                color: #181A1C;
-                transform: scale(0.8);
-              }
-              
-              .swiper-button-next {
-                padding: 10px 15px 10px 21px;
-              }
-              
-              .swiper-button-prev {
-                padding: 10px 21px 10px 15px;
-              }
-              `,
-            ],
-        };
-
-        Object.assign(swiperContainer, params);
-        swiperContainer.initialize();
-    }, []);
 
     return (
         <div className={props.className}>
-            <swiper-container ref={swiperRef} init="false">
+            <swiper-container centered-slides="true" slides-per-view="2" grab-cursor="true">
                 {
                     props.imagePaths.map(imagePath =>
                         <swiper-slide key={imagePath}>
