fed56233348052c535795e3e307e8a9eca020f00
Tim <timwagner997@gmail.com>
2024-02-01 21:42:09 +0100

    added media query to swipers for small and big screens

--- src/widgets/AppGallery.js
+++ src/widgets/AppGallery.js
@@ -1,7 +1,39 @@
+import {useEffect, useRef, useState} from "react";
+
 function AppGallery(props) {
+    const swiperRef = useRef(null);
+
+    const [isBigScreen, setIsBigScreen] = useState(
+        window.matchMedia("(min-width: 768px)").matches
+    )
+
+    useEffect(() => {
+        window
+            .matchMedia("(min-width: 768px)")
+            .addEventListener('change', e => setIsBigScreen( e.matches ));
+
+        const swiperContainer = swiperRef.current;
+
+        const params = {
+            centeredSlides: true,
+            initialSlide: props.initialSlide,
+            slidesPerView: isBigScreen ? 3 : 1.75,
+            grabCursor: true,
+            spaceBetween: 20,
+            slidesOffsetBefore: isBigScreen ? 130 : 0
+        };
+
+        if (isBigScreen) {
+            params.initialSlide ++
+        }
+
+        Object.assign(swiperContainer, params);
+        swiperContainer.initialize();
+    });
+
     return (
         <div className={props.className}>
-            <swiper-container centered-slides={true} initial-slide={props.initialSlide} slides-per-view="1.75" grab-cursor="true" space-between="20">
+            <swiper-container ref={swiperRef} init={false}>
                 {
                     props.imagePaths.map(imagePath =>
                         <swiper-slide key={imagePath}>
