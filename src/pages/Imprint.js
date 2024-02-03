92f17799376178d8e3c162a53c47bca9bc84be51
Tim <timwagner997@gmail.com>
2024-02-03 12:18:31 +0100

    changed imprint text

--- src/pages/Imprint.js
+++ src/pages/Imprint.js
@@ -5,7 +5,7 @@ import {useEffect} from "react";
 function Imprint() {
     useEffect(() => window.scrollTo(0, 0));
 
-    return(
+    return (
         <PageTemplate>
             <DismissTopBar title='Impressum'/>
             <div className='p-5 pt-20'>
@@ -31,27 +31,28 @@ function Imprint() {
 
                 <p className='font-bold my-5'>Haftung für Inhalte</p>
 
-                <p>Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit
-                    und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir
-                    gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen
-                    verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet,
-                    übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen,
-                    die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der
-                    Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine
-                    diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten
-                    Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese
-                    Inhalte umgehend entfernen.</p>
+                <p>
+                    Ich habe die Inhalte dieser Webseite mit größter Sorgfalt erstellt. Dennoch kann ich keine Gewähr
+                    für die Richtigkeit, Vollständigkeit und Aktualität der Informationen übernehmen. Als Betreiber
+                    dieser Webseite bin ich gemäß § 7 Abs. 1 TMG für meine eigenen Inhalte verantwortlich. Gemäß §§ 8
+                    bis 10 TMG bin ich jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu
+                    überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.
+                    Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen
+                    Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der
+                    Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden
+                    Rechtsverletzungen werde ich diese Inhalte umgehend entfernen.
+                </p>
 
                 <p className='font-bold my-5'>Haftung für Links</p>
 
-                <p>Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss
-                    haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte
-                    der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die
-                    verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft.
-                    Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente
-                    inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer
-                    Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links
-                    umgehend entfernen.</p>
+                <p>Ich biete auf meiner Webseite Links zu externen Webseiten Dritter an. Für die Inhalte dieser fremden
+                    Seiten übernehme ich jedoch keine Gewähr. Verantwortlich für die Inhalte der verlinkten Seiten ist
+                    jeweils der entsprechende Anbieter oder Betreiber. Zum Zeitpunkt der Verlinkung habe ich die
+                    verknüpften Seiten auf mögliche Rechtsverstöße überprüft und es waren keine rechtswidrigen Inhalte
+                    erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete
+                    Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Sollten mir Rechtsverletzungen bekannt werden,
+                    werde ich derartige Links umgehend entfernen.
+                </p>
             </div>
         </PageTemplate>
     )
