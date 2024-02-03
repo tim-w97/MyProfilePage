19a90d956c92d49593f026e5ce3c487d25ac9103
Tim <timwagner997@gmail.com>
2024-02-03 13:25:01 +0100

    deleted dynamic viewport height from 404 page

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -5,7 +5,7 @@ function PageNotFound() {
     return (
         <PageTemplate>
             <DismissTopBar title='Fehler 404'/>
-            <div className='p-5 pt-20 h-dvh'>
+            <div className='p-5 pt-20'>
                 <p>
                     Diese Seite gibt es vielleicht in einem anderen Paralleluniversum, aber nicht hier, tut mir Leid. 🥲
                 </p>
