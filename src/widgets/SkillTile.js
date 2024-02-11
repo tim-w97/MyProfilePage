c9c03c351e43d7820b72398968b8d2b76e3d9c2c
Tim <timwagner997@gmail.com>
2024-02-12 00:12:29 +0100

    replaced swiper of skill tiles with state (simpler approach)

--- src/widgets/SkillTile.js
+++ src/widgets/SkillTile.js
@@ -1,34 +1,19 @@
-import {useEffect, useRef} from "react";
-
 function SkillTile(props) {
-    const swiperRef = useRef(null);
-
-    useEffect(() => {
-        const swiperContainer = swiperRef.current;
-
-        Object.assign(swiperContainer);
-        swiperContainer.initialize();
-    });
-
     function flipTile() {
-        swiperRef.current.swiper.slideNext()
-        props.onTileFlip()
+        props.onTileFlip(props.index)
     }
 
     return (
         <div className='aspect-square m-3 cursor-pointer' onClick={flipTile}>
-            <swiper-container ref={swiperRef} init={false}>
-                <swiper-slide>
-                    <div className='bg-tim-gunpowder p-5'>
-                        <img className='aspect-square object-contain' src={props.img} alt={props.skill}/>
-                    </div>
-                </swiper-slide>
-                <swiper-slide>
-                    <div className='bg-tim-gray aspect-square flex items-center justify-center p-2'>
-                        <p className='text-center'>{props.skill}</p>
-                    </div>
-                </swiper-slide>
-            </swiper-container>
+            {props.isFlipped ?
+                <div className='bg-tim-gray aspect-square flex items-center justify-center p-2'>
+                    <p className='text-center'>{props.skill}</p>
+                </div>
+                :
+                <div className='bg-tim-gunpowder p-5'>
+                    <img className='aspect-square object-contain' src={props.img} alt={props.skill}/>
+                </div>
+            }
         </div>
     );
 }
--- src/widgets/Skills.js
+++ src/widgets/Skills.js
@@ -4,26 +4,9 @@ import flutterIcon from '../assets/images/skills/flutter.png'
 import swiftIcon from '../assets/images/skills/swift.png'
 import reactIcon from '../assets/images/skills/react.png'
 import javaIcon from '../assets/images/skills/duchess.png'
+import {useState} from "react";
 
 function Skills(props) {
-    async function onTileFlip(){
-        const swiperContainers =
-            document.querySelectorAll("#skill-tiles swiper-container")
-
-        const activeIndicesSum = Array.from(swiperContainers)
-            .map(swiperContainer => swiperContainer.swiper.activeIndex)
-            .reduce((partialSum, a) => partialSum + a, 0);
-
-        if (activeIndicesSum === 4) {
-            await sleep(1000)
-            swiperContainers.forEach(container => container.swiper.slideTo(0))
-        }
-    }
-
-    function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-    }
-
     const skills = [
         {
             img: flutterIcon,
@@ -43,14 +26,58 @@ function Skills(props) {
         }
     ]
 
+    const [tilesState, setTilesState] = useState(
+        Array(skills.length).fill(false)
+    )
+
+    async function setTileFlipped(index, state) {
+        const newState = [...tilesState]
+        newState[index] = state
+
+        let flippedCount = 0
+
+        for (const flipped of newState) {
+            if (flipped) {
+                flippedCount ++
+            }
+        }
+
+        setTilesState(newState)
+
+        if (flippedCount === skills.length) {
+            await sleep(1000)
+            resetTiles()
+        }
+    }
+
+    function resetTiles() {
+        setTilesState(
+            Array(skills.length).fill(false)
+        )
+    }
+
+    async function onTileFlip(index){
+        await setTileFlipped(index, true)
+    }
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
     return (
         <div className={props.className}>
             <div id="anchor-tools" className="absolute -top-20"></div>
             <p className="text-2xl font-bold text-center">Skills</p>
             <div id='skill-tiles' className="max-w-80 m-auto grid grid-cols-2">
-                {skills.map(skill =>
-                    <SkillTile key={skill.description} img={skill.img} skill={skill.description}
-                               onTileFlip={onTileFlip}/>
+                {skills.map((skill, index) =>
+                    <SkillTile
+                        key={skill.description}
+                        index={index}
+                        img={skill.img}
+                        skill={skill.description}
+                        isFlipped={tilesState[index]}
+                        onTileFlip={onTileFlip}
+                    />
                 )}
             </div>
         </div>
