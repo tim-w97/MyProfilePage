919b45d04fba3af2fc8091c8566f199e5f4d7a90
Tim <timwagner997@gmail.com>
2024-02-12 10:12:52 +0100

    changed style of tooltip

--- src/widgets/SkillTile.js
+++ src/widgets/SkillTile.js
@@ -1,14 +1,24 @@
 import {Tooltip} from "react-tooltip";
 
 function SkillTile(props) {
+    const tooltipStyle = {
+        backgroundColor: '#7BC7F3',
+        color: '#181A1C'
+    }
+
     return (
-        <div className='aspect-square m-3 cursor-pointer'>
+        <div className='aspect-square m-3'>
             <div
                 data-tooltip-id={`tooltip-${props.id}`}
                 data-tooltip-content={props.skill}
                 className='rounded-md bg-tim-gunpowder p-5'
             >
-                <Tooltip id={`tooltip-${props.id}`} />
+                <Tooltip
+                    id={`tooltip-${props.id}`}
+                    style={tooltipStyle}
+                    opacity={1}
+                    place='top'
+                />
                 <img className='aspect-square object-contain' src={props.img} alt={props.skill}/>
             </div>
         </div>
