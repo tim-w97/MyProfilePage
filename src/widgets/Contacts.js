ec1e2d10987d016e278ea18aac4f00e096800da0
Tim <timwagner997@gmail.com>
2024-02-19 22:52:42 +0100

    add instagram link to Flo

--- src/widgets/Contacts.js
+++ src/widgets/Contacts.js
@@ -1,6 +1,7 @@
 import githubLogo from "../assets/images/logos/github.png";
 import linkedinLogo from "../assets/images/logos/linkedin.png";
 import mailIcon from '../assets/images/icons/email.png'
+import instagram from '../assets/images/logos/instagram.png'
 import SendEmailLink from "./SendEmailLink";
 import {Link} from "react-router-dom";
 
@@ -12,31 +13,31 @@ function Contacts(props) {
       <ul className="list-none">
         <li>
           <a
-            className="flex bg-[#161B22] items-center cursor-pointer"
-            href="https://github.com/tim-w97"
-            target="_blank"
-            rel="noreferrer"
+              className="flex bg-[#161B22] items-center cursor-pointer"
+              href="https://github.com/tim-w97"
+              target="_blank"
+              rel="noreferrer"
           >
-            <img src={githubLogo} alt='Github Logo' className="w-16 h-16 object-contain ml-3 my-3 mr-8 shrink-0" />
+            <img src={githubLogo} alt='Github Logo' className="w-16 h-16 object-contain ml-3 my-3 mr-8 shrink-0"/>
             <p className="text-xl font-mono text-[#f0f6fc]">GitHub</p>
           </a>
         </li>
 
         <li>
           <a
-            className="flex bg-[#fff] items-center cursor-pointer"
-            href="https://www.linkedin.com/in/tim97"
-            target="_blank"
-            rel="noreferrer"
+              className="flex bg-[#fff] items-center cursor-pointer"
+              href="https://www.linkedin.com/in/tim97"
+              target="_blank"
+              rel="noreferrer"
           >
-            <img src={linkedinLogo} alt='Linkedin Logo' className="w-16 h-16 object-contain ml-3 my-3 mr-8 shrink-0" />
+            <img src={linkedinLogo} alt='Linkedin Logo' className="w-16 h-16 object-contain ml-3 my-3 mr-8 shrink-0"/>
             <p className="text-xl font-mono text-[#0a66c2]">LinkedIn</p>
           </a>
         </li>
 
         <li>
           <SendEmailLink className="flex bg-tim-dark-blue items-center cursor-pointer">
-            <img src={mailIcon} alt='mail icon' className="w-16 h-16 object-contain ml-3 my-3 mr-8 shrink-0" />
+            <img src={mailIcon} alt='mail icon' className="w-16 h-16 object-contain ml-3 my-3 mr-8 shrink-0"/>
             <p className="text-xl text-tim-light-blue">E-Mail schreiben</p>
           </SendEmailLink>
         </li>
@@ -44,8 +45,20 @@ function Contacts(props) {
         <div className="bg-tim-light-blue h-[1px] opacity-50"></div>
 
         <li>
-          <div className='flex flex-col md:flex-row items-center justify-between min-h-16 gap-3 p-5'>
-            <p className="font-normal">Mit viel ♥️ und React erstellt</p>
+          <div className='flex flex-col md:flex-row items-center justify-between gap-3 mx-5 mt-5'>
+            <p className="font-normal text-center">Portraitfotos aufgenommen von Florian</p>
+            <a href='https://www.instagram.com/flo.luell/' target='_blank'>
+              <div className='flex gap-3 items-center'>
+                <img className='aspect-square w-8' src={instagram} alt='Instagram Logo'/>
+                <p>@flo.luell</p>
+              </div>
+            </a>
+          </div>
+        </li>
+
+        <li>
+          <div className='flex flex-col md:flex-row items-center justify-between gap-3 mx-5 mt-10 md:mt-5 pb-5'>
+            <p className="font-normal text-center">Mit viel ♥️ und React erstellt</p>
             <Link to='/impressum' className="text-tim-light-blue">Impressum</Link>
           </div>
         </li>
