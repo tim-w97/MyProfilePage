998ce02e3f100f95fc1887c1b72a6d8792a41cc1
Tim <timwagner997@gmail.com>
2024-03-10 12:32:31 +0100

    added address to cv page

--- src/widgets/Profile.js
+++ src/widgets/Profile.js
@@ -4,6 +4,7 @@ import CallMeLink from "./CallMeLink";
 import tim from '../assets/images/tim/tim.jpg'
 import phoneIcon from '../assets/images/icons/phone.png'
 import mailIcon from '../assets/images/icons/mail.png'
+import homeIcon from '../assets/images/icons/home_pin.png'
 
 function Profile(props) {
   const birthday = moment('11091997', 'DDMMYYYY')
@@ -20,21 +21,29 @@ function Profile(props) {
             alt="Portrait von Tim"
         ></img>
         <p className="text-2xl font-bold my-3">Tim Wagner</p>
-        <p className="text-tim-gray">
+        <p className="text-tim-gray font-bold mb-10">
           geboren am 11.09.1997{" "}
           <span className="whitespace-nowrap">
           ({age} Jahre alt)
         </span>
         </p>
-        <p className="text-tim-gray">Wohnort: Hof (Bayern)</p>
+
         <div className="flex flex-col mt-3 gap-5 items-center">
           <SendEmailLink className='flex min-[350px]:flex-row flex-col gap-3 items-center'>
             <img className='w-12 aspect-square object-contain' src={mailIcon} alt='mail icon'/>
-            <p className='font-bold text-tim-light-blue'>timwagner997@gmail.com</p>
+            <p className='text-tim-light-blue'>timwagner997@gmail.com</p>
           </SendEmailLink>
+
+
+          <div className='flex min-[350px]:flex-row flex-col gap-3 items-center'>
+            <img className='w-12 aspect-square object-contain' src={homeIcon} alt='home pin icon'/>
+            <p className='text-tim-light-blue'>Königstr. 62, 95028 Hof</p>
+          </div>
+
+
           <CallMeLink className='flex min-[350px]:flex-row flex-col gap-3 items-center'>
             <img className='w-12 aspect-square object-contain' src={phoneIcon} alt='phone icon'/>
-            <p className='font-bold text-tim-light-blue'>01520-4847344</p>
+            <p className='text-tim-light-blue'>01520-4847344</p>
           </CallMeLink>
         </div>
       </div>
